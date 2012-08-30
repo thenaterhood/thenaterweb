05581d4e8865291818d19155a2e7122d36ce470e
nate <>
2012-08-30 06:19:15 +0000

    Removed extraneous newlines, changed font, and added privacy links

--- projects/index.php
+++ projects/index.php
@@ -1,8 +1,5 @@
-
 <?php 
-
 // Picks up any variables
-
 $first_name = $_GET['name'];
 $track = $_GET['track'];
 $konami = $_GET['konami'];
@@ -13,94 +10,53 @@ setcookie('visitor_name',$first_name,time() + (86400 * 30),"/","thenaterhood.com
 setcookie('visitor_name',$first_name,time() + (86400 * 30),"/","natelevesque.com"); // 86400 = 1 day
 setcookie('track',$track,time() + (86400 * 30),"/","thenaterhood.com"); // 86400 = 1 day
 setcookie('track',$track,time() + (86400 * 30),"/","natelevesque.com"); // 86400 = 1 day
-
 // Sets page options and variables
 if (empty($first_name)) $first_name = "Guest";
-if (empty($track)) $track = "";
+if (empty($track)) $track = "y";
 $id = $_GET['id'];
 if (empty($id)) $id = "home";
 $project_content_file = "projects_$id.html";
 $page_description_file = "page_$id_description.txt";
 if ("$id" == "home") $id = "projects";
 $page_keywords_file = "page_$id_keywords.txt";
-
 if ("$track" != "n") $tracking = "../template_tracking.php";
-
+if ("$track" == "n") $tracking = "../template_no-track.php";
 ?>
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-
 <html xmlns="http://www.w3.org/1999/xhtml">
-
 <head>
-
 <?php print '<meta name="keywords" content="nate levesque, thenaterhood" />'; ?>
-
 <?php print '<meta name="description" content="Nate Levesque" />'; ?>
-
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-
 <title><?php print "Nate Levesque :: $id"; ?>
-
 </title>
-
 <link href="/style.css" rel="stylesheet" type="text/css" media="screen" />
-
-<link href='http://fonts.googleapis.com/css?family=Abel' rel='stylesheet' type='text/css' />
-
+<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro' rel='stylesheet' type='text/css' />
 <style type="text/css">
-
 </style>
-
 <?php include "$tracking"; ?>
-
 </head>
-
 <?php if ("$konami" == "pride") print '<style type="text/css">body {background: url(/images/rainbow.jpg) fixed}</style>'; ?>
 <body>
-
 <div id="wrapper">
-
 <?php include "../template_header.php";?>
-
 	<div id="page">
-
-	
-
 		<div id="content">
-
 			<div class="post">
-
-
-
 				<div style="clear: both;">&nbsp;</div>
-
 				<div class="entry">
-
 				<?php if ("$first_name" == "Guest") include "../template_introduction.php"; ?>
-
 				<?php if (file_exists("$project_content_file")) include "$project_content_file"; else include "../template_error.php"; ?>
-
 				</div>
-
 			</div>
-
 		<div style="clear: both;">&nbsp;</div>
-
 		</div>
-
 		<!-- end #content -->
-
 <?php include "../template_sidebar.php"; ?> 
-
 	</div>
-
 </div>
-
 <?php include "../template_footer.php"; ?>
-
 </body>
-
 </html>
 
 
