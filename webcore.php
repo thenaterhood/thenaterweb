e11355d22652cd39c8e11f77dd0b842e959a3c67
Nate Levesque <public@thenaterhood.com>
2012-12-30 18:19:03 -0500

    copy all common code to webcore to include

---
+++ webcore.php
@@ -0,0 +1,119 @@
+<?php
+
+function safeChars($string, $length) {
+    /*
+     * Verify that a string is made of html-safe characters and
+     * short enough to fit where it belongs.  Basically some simple
+     * input sanitizing for nonsecure things.
+     * 
+     * Arguments:
+     *  $string (string): a string or something else
+     *  $length (integer): an integer value for the length limit of the string
+     * 
+     * Returns:
+     *  $safestring (string): a html-safe and proper length string
+     */
+     
+    # Check that the string is actually a string, return "" if not
+    if (gettype($string) != 'string'){
+        return '';
+    }
+    
+    #Santize input so that it's text so we don't have XSS problems
+    $safestring = htmlspecialchars($string, ENT_QUOTES);
+    
+    #Check the length of the string and the limit given, truncate if needed
+    if ($length == 0){
+        return $safestring;
+    }
+    if (strlen($safestring) > $length){
+        return substr($safestring, 0, $length);
+    }
+    else {
+        return $safestring;
+    }
+}
+
+function setIfEmpty($string, $emptyValue){
+	/*
+	 * Checks if a given string is empty and returns the value to set
+	 * it as if it is.  if not, returns the string.
+	 *
+	 * Arguments:
+	 *	$string (string): string value to check
+	 *	$emptyValue (string): Value to return if the string is empty
+	 *
+	 * Returns:
+	 *	$string or $emptyValue (string): $string if the string is not empty
+	 *		or $emptyValue if the string is empty
+	 */
+	if (empty($string)){
+		return $emptyValue;
+	}
+	else{
+		return $string;
+	}
+}
+
+function checkCookie($name, $emptyValue){
+	/*
+	 * Checks the cookie with the given name and returns its contents,
+	 * or a default value if the cookie is empty/doesn't exist
+	 * 
+	 *
+	 * Arguments:
+	 *	$name (string): the name of the cookie to check
+	 *	$emptyValue (string): string to return if the cookie is bad
+	 *
+	 * Returns:
+	 *	$contents (string): the contents of the cookie or default value
+	 */
+	$contents = $_COOKIE[$name];
+	
+	return setIfEmpty(&$contents, &$emptyValue);
+}
+
+function setVarFromURL($name, $emptyValue, $length){
+	/*
+	 * Sets a variable from the URL by running the URL input through
+	 * safeChars to make it html-safe and the right size, then
+	 * looking for a cookie if the variable has not been set, and 
+	 * sets the variable to a default value if it has not been defined
+	 * in the url or a cookie.
+	 *
+	 * Arguments:
+	 *	$name (string): the name of the variable to get/set
+	 *	$emptyValue (string): a default value for the variable if no
+	 *		other value can be found
+	 *	$length (int): a maximum length for the variable if pulled from URL
+	 *
+	 * Returns:
+	 *	(string): the default value or the value pulled from a cookie or URL
+	 */
+	$value = safeChars($_GET[$name], &$length);
+	return setIfEmpty(&$value, checkCookie(&$name, &$emptyValue));
+}
+
+function chooseInclude($preferred, $secondary){
+    /*
+     * Checks to see if the preferred file exists, and if it does
+     * returns it, otherwise it returns the secondary file, which ideally
+     * should be a file (like an error page) that is guaranteed to exist.
+     * 
+     * Arguments:
+     *  $preferred (string): the preferred page to include
+     *  $secondary (string): the secondary, emergency page to include
+     * 
+     * Returns:
+     *  $page (string): the page that should be included
+     * 
+    */
+    
+    if (file_exists("$preferred")){
+        return "$preferred";
+    }
+    else{
+        return "$secondary";
+    }
+}
+?>
