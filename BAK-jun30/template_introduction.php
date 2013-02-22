882587171154478b068ddaaffc2643bfdb78bddc
Nate Levesque <public@thenaterhood.com>
2013-02-21 19:24:32 -0500

    Merge cleanup, remove unneeded files

--- BAK-jun30/template_introduction.php
+++
@@ -1,6 +0,0 @@
-<table border="0">
-<tr>
-<td valign="top"><p>  I don't think we've met. What's your name?</p></td>
-<td valign="top"><form id="introduction" action="index.php" method="get"><p><input type="text" name="name" /><input type="submit" value="Introduce me!"/></p></form></td>
-<td><p> <a href="?id=privacy">(privacy)</a></p></td></tr>
-</table>
--- template_sidebar.php
+++
@@ -1,6 +0,0 @@
-<div id="sidebar">
-			<ul><li></li>
-			</ul>
-		</div>
-		<!-- end #sidebar -->
-		<div style="clear: both;">&nbsp;</div>
