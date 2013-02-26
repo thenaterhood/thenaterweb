f98ba4a7e1a8688e0f50d66803f9a3c5f9ae0859
Nate Levesque <public@thenaterhood.com>
2013-02-25 23:16:45 -0500

    add disqus comment code

--- blog/post.php
+++ blog/post.php
@@ -50,21 +50,42 @@ else {
 				$displaypost = new postObj( $config->post_directory.'/'.$session->node );
 				print $displaypost->page_output();
 				
-				if ( $displaypost->datestamp ){
+				if ( True ){
 					/*
 					 * Shows suggestions for other posts only if the 
 					 * current post being shown happens to exist- ie, 
 					 * it has a datestamp. Since datestamps will be automatically
 					 * added to posts, all posts will have a datestamp.
 					 */
-					echo '<h5>If you liked this, you might also like...</h5>';
+					echo '<h5>You might also be interested in:</h5>';
 					echo '<ul>';
 					getSuggestions(3, $displaypost->tags);
 					echo '</ul>';
 					echo '<p><a href="index.php">Back to Blog Home</a></p>';
 				}
-				?>
+				if ( $displaypost->datestamp ){
+					echo "
+				<!-- BEGIN DISQUS CODE -->
+				<div id='disqus_thread'></div>
+				<script type='text/javascript'>
+				/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+				var disqus_shortname = 'nphilosophy'; // required: replace example with your forum shortname
+
+				/* * * DON'T EDIT BELOW THIS LINE * * */
+				(function() {
+					var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+					dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+					(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+				})();
+				</script>
+				<noscript>Please enable JavaScript to view the <a href='http://disqus.com/?ref_noscript'>comments powered by Disqus.</a></noscript>
+				<a href='http://disqus.com' class='dsq-brlink'>comments powered by <span class='logo-disqus'>Disqus</span></a>
+    
 				</div>
+				<!-- END DISQUS CODE -->";
+				
+				}
+				?>
 		</div>
 		<!-- end #content -->
 	<div style="clear:both;">&nbsp;</div>
