58af082decaa4b792ad97461bb24fae7bc330e4c
Nate Levesque <public@thenaterhood.com>
2013-04-10 15:20:44 -0400

    updates to unit tests

--- testing/testEngine.py
+++ testing/testEngine.py
@@ -24,41 +24,37 @@ class testWebEngine(unittest.TestCase):
 		
 	def test_sanitizeShort(self):
 		# Test the ability to sanitize a short string
-		page = urlopen( url+"api_saneString.php?testvar=justtest" ).read().decode('utf-8')
-		decodedData = json.loads( page )
+		decodedData = getData( url+"api_saneString.php?testvar=justtest" )
 		self.assertEqual( decodedData["testvar"], "justtest" )
 		
 	def test_sanitizeMessy(self):
 		# Test the ability to remove non-alphanumberic characters
-		page = urlopen( url+"api_saneString.php?testvar=:)just*test$*@(*%" ).read().decode('utf-8')
-		decodedData = json.loads( page )
+		decodedData = getData( url+"api_saneString.php?testvar=:)just*test$*@(*%" )
 		self.assertEqual( decodedData["testvar"], "justtest" )
 		
 	def test_sessionDomain(self):
 		# Test that the session class contains expected data
-		page = urlopen( url+"api_session.php" ).read().decode('utf-8')
-		decodedData = json.loads( page )
+		decodedData = getData( url+"api_session.php" )
 		self.assertEqual( decodedData["domain"], domain )
 		
 	def test_sessionData(self):
 		# Test that the session class can pick up data
-		page = urlopen( url+"api_session.php?name=test").read().decode('utf-8')
-		decodedData = json.loads(page)
+		decodedData = getData( url+"api_session.php?name=test" )
 		self.assertEqual( decodedData["name"], "test" )
 		
 	def test_sessionDataMessy(self):
 		# Test that the session class can pick up messy data correctly
-		page = urlopen( url+"api_session.php?name=te$%@!st").read().decode('utf-8')
-		decodedData = json.loads(page)
+		decodedData = getData( url+"api_session.php?name=te$%@!st&konami=pride" )
 		self.assertEqual( decodedData["name"], "test" )
 		
 	def test_postObj_missing(self):
 		# Test that the postObj class can return data and deal with missing posts
-		page = urlopen( url+"api_post.php?node=test&element=title").read().decode('utf-8')
-		decodedData = json.loads(page)
+		decodedData = getData( url+"api_post.php?node=test&element=title" )
 		self.assertEqual( decodedData["title"], "Oops! Post Not Found!" )
 
-
+def getData( address ):
+	page = urlopen( address ).read().decode('utf-8')
+	return json.loads(page)
 
 if __name__ == "__main__":
     unittest.main()
