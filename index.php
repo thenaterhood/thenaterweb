6491a2a6c649f4be6b60cf89ccdbade69872dd9e
Nate Levesque <public@thenaterhood.com>
2013-04-22 11:13:38 -0400

    docblock and head now constructed by page class

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ include 'engine/core_redirect.php';
 
 $session = new session( array('name', 'track', 'konami', 'id', 'type') );
 $config = new config();
+$page = new page( $session->id );
 
 # Grab variables from the URL. Syntax for this is...
 # name of variable, default value of variable, maxlength of variable
@@ -27,32 +28,9 @@ setcookie('track',$track,time() + (86400 * 30),"/",$session->domain); // 86400 =
 $page_content_file = "page_$id.html";
 	
 include $config->webcore_root.'/html_doctype.html';
-?>
 
-<head>
-<meta name="keywords" content='Nate Levesque, TheNaterhood, the naterhood' />
-<?php echo '<meta name="description" content="Nate Levesque" />'; ?>
-<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-<title><?php echo "Nate Levesque :: $id"; ?></title>
-<link href="/style.css" rel="stylesheet" type="text/css" media="screen" />
-<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro' rel='stylesheet' type='text/css' />
-<link href='http://fonts.googleapis.com/css?family=Electrolize' rel='stylesheet' type='text/css' />
-<style type="text/css">
-</style>
-<?php 
-/*
- * Inserts any tracking code into the page if the user hasn't
- * requested to not be tracked.
- */
-if ($session->track == "n"){
-	print "<!-- Tracking code removed from page by request -->";
-}
-else {
-	print $config->tracking_code;
-}
+$page->display();
 ?>
-<?php if ($session->konami == "pride") print '<style type="text/css">body {background: url(images/rainbow.jpg) fixed}</style>'; ?>
-</head>
 <body>
 <div id="wrapper">
 <?php include chooseInclude( $config->webcore_root.'/template_header.php', 'layout_error.html');?>
