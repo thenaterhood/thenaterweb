eb88d34f69417b14982158adc9603fde9ca76c94
Nate Levesque <public@thenaterhood.com>
2013-05-24 02:09:04 -0400

    migrate feed functions to engine

--- engine/core_feed.php
+++ engine/core_feed.php
@@ -23,16 +23,40 @@
  * @return $atom (atom_feed): an instance of the atom_feed class
  * 
  */
-function generateFeed( $bloguri, $feedTitle, $feedCatchline ){
+function generateFeed( $bloguri, $feedTitle, $feedCatchline, $forceRegen ){
+
 	
 	$inventory = new inventory( getConfigOption('post_directory'), $bloguri );
 	$posts = $inventory->getFileList();
-	$atom = new feed($feedTitle, getConfigOption('site_domain').'/'.$bloguri, $feedCatchline, date(DATE_ATOM) );
-	
-	for ($i = 0; $i < count($posts); $i++){
-		$newitem = new article(getConfigOption('post_directory').'/'.$posts[$i], $bloguri );
-		$atom->new_item($newitem);
+
+	$atom = new feed( $bloguri );
+
+	if ( ! getConfigOption('auto_feed_regen') && ! $forceRegen && $atom->exists() ){
+		/*
+		* If the inventory matches the existing number of items in the
+		* directory, return the static feed file
+		*/
+		return $atom;
 	}
+
+	else{
+		/*
+		* If the inventory doesn't match the existing number of items in
+		* the directory, regenerate the inventory and the feed file
+		* then return the feed file
+		*/
+		if ( $forceRegen || ! $inventory->current() || ! $atom->exists() ){
+			$inventory->regen();
+			$atom->reset( $bloguri, $feedTitle, $feedCatchline,  date(DATE_ATOM) );
+
+			for ($i = 0; $i < count($posts); $i++){
+				$newitem = new article(getConfigOption('post_directory').'/'.$posts[$i], $bloguri );
+				$atom->new_item($newitem);
+			}
+
+			$atom->save();
+		}
+
 	return $atom;
 }
 
