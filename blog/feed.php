d08e99480a35e1776d3aa45e0caa6a4e99d1fb5e
Nate Levesque <public@thenaterhood.com>
2013-05-24 02:22:07 -0400

    fixes to coding errors

--- blog/feed.php
+++ blog/feed.php
@@ -6,6 +6,6 @@ $session = new session( array('regen') );
 $config = new config();
 
 $feed = generateFeed( 'blog', 'The Philosophy of Nate', "It's the cyber age; stay in the know", $session->regen );
-$feed->output( $config->feed_type );
+print $feed->output( $config->feed_type );
 
 ?>
