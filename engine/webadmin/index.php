a44dc1b8ec4cf078f88587670ec64c1f506c77d6
Nate Levesque <public@thenaterhood.com>
2013-05-31 17:56:35 -0400

    finish migration to .htpasswd

--- engine/webadmin/index.php
+++ engine/webadmin/index.php
@@ -1,13 +1,6 @@
 <?php
 include '../core_web.php';
-include '../class_webAdmAuth.php';
-include '../class_redirect.php';
 
-$session = new session( array( 'user', 'pass', 'id', 'active' ) );
-
-$auth = new webAdmAuth( $session->user, $session->pass, $session->active );
-
-if ( $auth->isAuthenticated() ){
 	print "Welcome $session->user. Administering to the site at ".getConfigOption('site_domain');
 	print '<ul>
 	<li><a href="newpost.php">Create a new Post or Article</a></li>
@@ -16,10 +9,5 @@ if ( $auth->isAuthenticated() ){
 	<li><a href="editblog.php">Edit an existing blog or article configuration</a></li>
 	<li><a href="editconf.php">Modify engine configuration</a></li>
 	</ul>';
-}
-else{
-	$redirect = new redirect( $session->uri, 'index.php' );
-	$redirect->apply( 302 );
-}
 
 ?>
--- engine/webadmin/newpost.php
+++ engine/webadmin/newpost.php
@@ -1,27 +1,14 @@
 <?php
 
 include '../core_blog.php';
-include_once '../class_redirect.php';
-include '../class_webAdmAuth.php';
 
-$session = new session( array( 'user', 'pass', 'active' ) );
-$auth = new webAdmAuth( $session->user, $session->pass, $session->active );
+print '<form name="create" action="savepost.php" method="post">
+Title: <input type="text" name="title" /><br />
+Tags: <input type="text" name="tags" /><br />
+Blog: <input type="text" name="blog" /><br />
+Write your post: <br /><textarea id="content" rows="1000" cols="1000" /><br />
+<input type="submit" value="Save" />
+</form>';
 
-if ( $auth->isAuthenticated() ){
-
-	print '<form name="create" action="savepost.php" method="post">
-    Title: <input type="text" name="title" /><br />
-    Tags: <input type="text" name="tags" /><br />
-    Blog: <input type="text" name="blog" /><br />
-    Write your post: <br /><textarea id="content" rows="1000" cols="1000" /><br />
-    <input type="submit" value="Save" />
-	</form>';
-}
-else{
-
-	$redirect = new redirect( $session->uri, 'index.php' );
-
-
-}
 
 ?>
