2381dcb4b5a097bb7c1c1477bd6ccaf8c73f1e03
Nate Levesque <public@thenaterhood.com>
2013-05-31 15:12:36 -0400

    add login functionality to webadmin

---
+++ engine/class_webAdmAuth.php
@@ -0,0 +1,33 @@
+<?php
+
+include_once 'class_dataMonger.php';
+include_once 'core_web.php';
+
+class webAdmAuth extends dataMonger{
+
+
+	
+	function __construct( $user, $pass, $active ){
+
+		$this->container['user'] = $user;
+		$this->container['pass'] = $pass;
+		$this->container['active'] = $active;
+		$this->container['shadowFile'] = '/var/www/shadow.json';
+
+		$shadow = json_decode( file_get_contents( $this->container['shadowFile'] ), True );
+
+		if ( $this->container['pass'] == $shadow[$user] ){
+			$this->container['active'] = True;
+		}
+
+	}
+
+	public function isAuthorized(){
+
+		return $this->container['active'];
+
+	}
+
+}
+
+?>
