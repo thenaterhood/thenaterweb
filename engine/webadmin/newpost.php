6e24f37179387bd96920bffb4c52c15547be3d51
Nate Levesque <public@thenaterhood.com>
2013-05-31 18:01:14 -0400

    fix issue with text area wayyy too big

--- engine/webadmin/newpost.php
+++ engine/webadmin/newpost.php
@@ -6,7 +6,7 @@ print '<form name="create" action="savepost.php" method="post">
 Title: <input type="text" name="title" /><br />
 Tags: <input type="text" name="tags" /><br />
 Blog: <input type="text" name="blog" /><br />
-Write your post: <br /><textarea id="content" rows="1000" cols="1000" /><br />
+Write your post: <br /><textarea id="content" rows="200" cols="200" /><br />
 <input type="submit" value="Save" />
 </form>';
 
