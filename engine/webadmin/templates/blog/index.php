156dd149c6cee1165e32ea7d14ed1cf3d3d7c7d9
Nate Levesque <public@thenaterhood.com>
2013-05-31 14:47:41 -0400

    add basic template files for webadmin, begin concept

---
+++ engine/webadmin/templates/blog/index.php
@@ -0,0 +1,53 @@
+<?php 
+include '../engine/core_blog.php';
+include 'class_blogdef.php';
+
+
+$session = new session( array('name', 'track', 'konami', 'start', 'end', 'id', 'test', 'tag', 'node') );
+$config = new config();
+$blogdef = new blogdef();
+
+# For compatibility with current header
+$id = $blogdef->id;
+
+include $config->webcore_root.'/html_doctype.html';
+include $config->webcore_root.'/html_head.html';
+?>
+
+<body>
+<div id="wrapper">
+<?php include chooseInclude( $config->webcore_root.'/template_header.php', '../layout_error.html');?>
+	<div id="page">
+		<div id="content">
+				<div style="clear: both;">&nbsp;</div>
+				
+				<?php include chooseInclude( 'static/template_blognav.php', '../layout_error.html'); ?>
+				
+				<div class="entry">
+					
+				<?php 
+				$include = getContent( 'static/page_'.$session->id, $config->webcore_root.'/template_error.php');
+
+				print $include['pre'];
+
+				if ( !$include['sanitize'] ){
+					include $include['file'];
+				}
+				else{
+					print htmlspecialchars( file_get_contents($include['file']) );
+				}
+
+				print $include['post'];
+				//$content = new content( $session->id, $session );
+				//$content->output();
+				?>
+					
+				</div>
+		</div>
+		<!-- end #content -->
+	<div style="clear:both;">&nbsp;</div>
+	</div>
+</div>
+<?php include chooseInclude( $config->webcore_root.'/template_footer.php', '../layout_error.html'); ?>
+</body>
+</html>
