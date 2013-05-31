3c12a7a1e4747559adfdc0e8044d3833c5f29e96
Nate Levesque <public@thenaterhood.com>
2013-05-31 19:11:09 -0400

    reorganization to make use of main engine

--- engine/webadmin/index.php
+++ engine/webadmin/index.php
@@ -1,13 +1,51 @@
-<?php
-include '../core_web.php';
+<?php 
+include '../engine/core_web.php';
 
-	print "Welcome $session->user. Administering to the site at ".getConfigOption('site_domain');
-	print '<ul>
-	<li><a href="newpost.php">Create a new Post or Article</a></li>
-	<li><a href="editpost.php">Edit an existing post or article</a></li>
-	<li><a href="newblog.php">Create a new blog or article collection</a></li>
-	<li><a href="editblog.php">Edit an existing blog or article configuration</a></li>
-	<li><a href="editconf.php">Modify engine configuration</a></li>
-	</ul>';
 
+$session = new session( array('name', 'track', 'konami', 'start', 'end', 'id', 'test', 'tag', 'node') );
+$config = new config();
+
+# For compatibility with current header
+$id = $session->id;
+
+include $config->webcore_root.'/html_doctype.html';
+include $config->webcore_root.'/html_head.html';
 ?>
+
+<body>
+<div id="wrapper">
+<?php include chooseInclude( $config->webcore_root.'/template_header.php', '../layout_error.html');?>
+	<div id="page">
+		<div id="content">
+				<div style="clear: both;">&nbsp;</div>
+				
+				<?php include chooseInclude( 'static/template_blognav.php', '../layout_error.html'); ?>
+				
+				<div class="entry">
+					
+				<?php 
+				$include = getContent( 'static/page_'.$session->id, $config->webcore_root.'/template_error.php');
+
+				print $include['pre'];
+
+				if ( !$include['sanitize'] ){
+					include $include['file'];
+				}
+				else{
+					print htmlspecialchars( file_get_contents($include['file']) );
+				}
+
+				print $include['post'];
+				//$content = new content( $session->id, $session );
+				//$content->output();
+				?>
+					
+				</div>
+		</div>
+		<!-- end #content -->
+	<div style="clear:both;">&nbsp;</div>
+	</div>
+</div>
+<?php include chooseInclude( $config->webcore_root.'/template_footer.php', '../layout_error.html'); ?>
+</body>
+</html>
