f7ff7e7897225f7831ca8b6ae889f9b293a9a6db
Nate Levesque <public@thenaterhood.com>
2013-05-31 17:24:40 -0400

    expand webadmin framework files for prospective functions

--- engine/webadmin/login.php
+++ engine/webadmin/login.php
@@ -13,6 +13,8 @@ if ( $auth->isAuthenticated() ){
 	<li><a href="newpost.php">Create a new Post or Article</a></li>
 	<li><a href="editpost.php">Edit an existing post or article</a></li>
 	<li><a href="newblog.php">Create a new blog or article collection</a></li>
+	<li><a href="editblog.php">Edit an existing blog or article configuration</a></li>
+	<li><a href="editconf.php">Modify engine configuration</a></li>
 	</ul>';
 }
 else{
