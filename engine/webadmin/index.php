b769df7962d4c4cd64441c7b10eec19c98e7d412
Nate Levesque <public@thenaterhood.com>
2013-05-31 17:51:14 -0400

    move php auth to htaccess, because easier

--- engine/webadmin/index.php
+++ engine/webadmin/index.php
@@ -1,15 +1,25 @@
 <?php
-	
 include '../core_web.php';
+include '../class_webAdmAuth.php';
+include '../class_redirect.php';
 
 $session = new session( array( 'user', 'pass', 'id', 'active' ) );
 
-if ( !$session->active ){
-	print '<form name="login" action="login.php" method="post">
-    Username: <input type="text" name="user" />
-    Password: <input type="password" name="pass" />
-    <input type="submit" value="Login" />
-	</form>';
+$auth = new webAdmAuth( $session->user, $session->pass, $session->active );
+
+if ( $auth->isAuthenticated() ){
+	print "Welcome $session->user. Administering to the site at ".getConfigOption('site_domain');
+	print '<ul>
+	<li><a href="newpost.php">Create a new Post or Article</a></li>
+	<li><a href="editpost.php">Edit an existing post or article</a></li>
+	<li><a href="newblog.php">Create a new blog or article collection</a></li>
+	<li><a href="editblog.php">Edit an existing blog or article configuration</a></li>
+	<li><a href="editconf.php">Modify engine configuration</a></li>
+	</ul>';
+}
+else{
+	$redirect = new redirect( $session->uri, 'index.php' );
+	$redirect->apply( 302 );
 }
 
 ?>
