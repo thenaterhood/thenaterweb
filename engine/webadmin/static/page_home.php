df345c38bc91e3fb38a4e1938b23cdb1d940f06c
Nate Levesque <public@thenaterhood.com>
2013-05-31 19:11:09 -0400

    reorganization to make use of main engine

---
+++ engine/webadmin/static/page_home.php
@@ -0,0 +1,13 @@
+<?php
+include '../core_web.php';
+
+	print "Welcome! Administering to the site at ".getConfigOption('site_domain');
+	print '<ul>
+	<li><a href="newpost.php">Create a new Post or Article</a></li>
+	<li><a href="editpost.php">Edit an existing post or article</a></li>
+	<li><a href="newblog.php">Create a new blog or article collection</a></li>
+	<li><a href="editblog.php">Edit an existing blog or article configuration</a></li>
+	<li><a href="editconf.php">Modify engine configuration</a></li>
+	</ul>';
+
+?>
