783a80da6974cf70c28c803021199080d36550aa
Nate Levesque <public@thenaterhood.com>
2013-05-31 18:03:29 -0400

    further fixes to size of textarea

--- engine/webadmin/newpost.php
+++ engine/webadmin/newpost.php
@@ -6,7 +6,7 @@ print '<form name="create" action="savepost.php" method="post">
 Title: <input type="text" name="title" /><br />
 Tags: <input type="text" name="tags" /><br />
 Blog: <input type="text" name="blog" /><br />
-Write your post: <br /><textarea id="content" rows="200" cols="200" /><br />
+Write your post: <br /><textarea id="content" rows="100" cols="100" ></textarea><br />
 <input type="submit" value="Save" />
 </form>';
 
