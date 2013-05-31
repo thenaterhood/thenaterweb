157147cd6f26622d5aa63804d9cbe166411fb687
Nate Levesque <public@thenaterhood.com>
2013-05-31 17:17:02 -0400

    add links to prospective functions

--- engine/webadmin/login.php
+++ engine/webadmin/login.php
@@ -9,6 +9,11 @@ $auth = new webAdmAuth( $session->user, $session->pass, $session->active );
 
 if ( $auth->isAuthenticated() ){
 	print "Welcome $session->user. Administering to the site at ".getConfigOption('site_domain');
+	print '<ul>
+	<li><a href="newpost.php">Create a new Post or Article</a></li>
+	<li><a href="editpost.php">Edit an existing post or article</a></li>
+	<li><a href="newblog.php">Create a new blog or article collection</a></li>
+	</ul>';
 }
 else{
 	$redirect = new redirect( $session->uri, 'index.php' );
