eeb3384de6f96ad34c70f1e0736f2c0b3d059d59
Nate Levesque <public@thenaterhood.com>
2013-06-01 00:51:25 -0400

    resize input area dynamically so we don't have nested scrolling

--- engine/webadmin/static/page_editconf.php
+++ engine/webadmin/static/page_editconf.php
@@ -1,6 +1,15 @@
 <h1>Edit Engine Configuration</h1>
 <p><strong>WARNING:</strong> proper PHP syntax must be observed, or your edits will crash the engine and will require fixing from an external tool!</p>
 <form action="index.php?id=saveconf" method="POST">
-<textarea name="content" rows="100" cols="100" ><?php print file_get_contents('../class_config.php'); ?></textarea><br />
+
+<?php
+
+$config = file_get_contents('../class_config.php');
+$lines = count( explode($config) );
+
+// Generate the input area, with the right height
+print '<textarea name="content" rows="'.print $lines.'" cols="100" >.'print $config.'</textarea>'
+?>
+<br />
 <input type="submit" value="Save and Apply" />
 </form>
