0c723aa72502fab73539e8a19081fdcf85521ff2
Nate Levesque <public@thenaterhood.com>
2013-06-01 00:02:59 -0400

    remove unneeded copypasta code

--- engine/webadmin/static/page_editconf.php
+++ engine/webadmin/static/page_editconf.php
@@ -1,11 +1,6 @@
 <h1>Edit Engine Configuration</h1>
+<p><strong>WARNING:</strong> proper PHP syntax must be observed, or your edits will crash the engine and will require fixing from an external tool!</p>
 <form name="create" action="index.php?id=saveconf" method="post">
-Title: <input type="text" name="title" /><br />
-Tags: <input type="text" name="tags" /><br />
-Blog: <input type="text" name="blog" /><br />
-Write your post: <br /><textarea id="content" rows="100" cols="100" ><?php print file_get_contents('../class_config.php'); ?></textarea><br />
+<textarea id="content" rows="100" cols="100" ><?php print file_get_contents('../class_config.php'); ?></textarea><br />
 <input type="submit" value="Save" />
 </form>
-<?php
-
-?>
