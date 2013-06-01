92363d87712a2d307d9ee0b4da56bbf8f6de2c75
Nate Levesque <public@thenaterhood.com>
2013-06-01 03:33:31 -0400

    implement functionality to create new blog from admin panel

--- engine/webadmin/static/page_newpost.php
+++ engine/webadmin/static/page_newpost.php
@@ -1,9 +1,10 @@
-<h1>Create a new article</h1>
+<h1>Edit An Article</h1>
 
 <form name="create" action="index.php?id=savepost" method="post">
-Title: <input type="text" name="title" /><br />
-Tags: <input type="text" name="tags" /><br />
-Blog: <input type="text" name="blog" /><br />
-Write your post: <br /><textarea id="content" rows="100" cols="100" ></textarea><br />
-<input type="submit" value="Save" />
+Title: <input type="text" name="title" /><?php print $_POST['title']; ?><br />
+Tags: <input type="text" name="tags" /><?php print $_POST['tags']; ?><br />
+Blog: <input type="text" name="blog" /><?php print $_POST['blogid']; ?><br />
+Write your post: <br /><textarea name="content" rows="100" cols="100" ><?php print $_POST['content']; ?></textarea><br />
+<input type="submit" value="Create" />
+
 </form>
