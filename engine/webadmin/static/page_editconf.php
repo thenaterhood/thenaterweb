529052af89b98a2810e27c181d11bb4e67290c55
Nate Levesque <public@thenaterhood.com>
2013-06-01 00:00:52 -0400

    add ability to display configuration

--- engine/webadmin/static/page_editconf.php
+++ engine/webadmin/static/page_editconf.php
@@ -1,4 +1,11 @@
 <h1>Edit Engine Configuration</h1>
+<form name="create" action="index.php?id=saveconf" method="post">
+Title: <input type="text" name="title" /><br />
+Tags: <input type="text" name="tags" /><br />
+Blog: <input type="text" name="blog" /><br />
+Write your post: <br /><textarea id="content" rows="100" cols="100" ><?php print file_get_contents('../class_config.php'); ?></textarea><br />
+<input type="submit" value="Save" />
+</form>
 <?php
 
 ?>
