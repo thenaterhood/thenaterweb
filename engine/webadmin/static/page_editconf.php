1b26a52d333059925a9d638b7b1a274d26826dd8
Nate Levesque <public@thenaterhood.com>
2013-06-01 00:37:03 -0400

    change form id->name since i can't html

--- engine/webadmin/static/page_editconf.php
+++ engine/webadmin/static/page_editconf.php
@@ -1,6 +1,6 @@
 <h1>Edit Engine Configuration</h1>
 <p><strong>WARNING:</strong> proper PHP syntax must be observed, or your edits will crash the engine and will require fixing from an external tool!</p>
-<form name="create" action="index.php?id=saveconf" method="post">
-<textarea id="content" rows="100" cols="100" ><?php print file_get_contents('../class_config.php'); ?></textarea><br />
-<input type="submit" value="Save" />
+<form action="index.php?id=saveconf" method="POST">
+<textarea name="content" rows="100" cols="100" ><?php print file_get_contents('../class_config.php'); ?></textarea><br />
+<input type="submit" value="Save and Apply" />
 </form>
