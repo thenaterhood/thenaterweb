f5740a3484e43e53cb56551dddffa4e0d389a457
Nate Levesque <public@thenaterhood.com>
2013-06-04 00:17:30 -0400

    update newpost to edit posts as well

--- engine/webadmin/static/page_newpost.php
+++ engine/webadmin/static/page_newpost.php
@@ -1,10 +1,11 @@
 <h1>Edit An Article</h1>
 
 <form name="create" action="index.php?id=savepost" method="post">
-Title: <input type="text" name="title" /><?php print $_POST['title']; ?><br />
-Tags: <input type="text" name="tags" /><?php print $_POST['tags']; ?><br />
-Blog: <input type="text" name="blog" /><?php print $_POST['blogid']; ?><br />
+Title: <input type="text" name="title" value="<?php print $_POST['title']; ?>" /><br />
+Tags: <input type="text" name="tags" value="<?php print $_POST['tags']; ?>" /><br />
+Blog: <input type="text" name="blog" value="<?php print $_POST['blogid']; ?>" /><br />
 Write your post: <br /><textarea name="content" rows="100" cols="100" ><?php print $_POST['content']; ?></textarea><br />
+<input type="hidden" name="file" value="<?php print $_POST['file']; ?>" />
 <input type="submit" value="Create" />
 
 </form>
