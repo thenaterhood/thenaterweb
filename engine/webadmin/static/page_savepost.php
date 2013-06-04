832091a60306e313a1755bf0b6e7e955c910399d
Nate Levesque <public@thenaterhood.com>
2013-06-04 01:29:48 -0400

    refinements to postsave script

--- engine/webadmin/static/page_savepost.php
+++ engine/webadmin/static/page_savepost.php
@@ -11,6 +11,7 @@
 	$postData['date'] = $_POST['date'];
 	$postData['tags'] = $_POST['tags'];
 	$postData['datestamp'] = date(DATE_ATOM);
+	$postData['updated'] = date(DATE_ATOM);
 
 	$postJsonData = json_encode($postData);
 
@@ -18,8 +19,18 @@
 	fwrite($jsonFile, $postJsonData);
 	fclose($jsonFile);
 
-	print '<h1>Post Saved</h1>';
-	print '<p>View this post at '.getConfigOption('site_domain').'/'.$_POST['blog'].'/'.$postFname.'</p>';
+	$postURL = getConfigOption('site_domain').'/'.$_POST['blog'].'/index.php?id=post&node='.date("Y.m.d");
+
+	if ( is_writeable( $postpath.'/'.$postFname ) ){
+		print '<h1>Post Saved</h1>';
+		print '<p>View this post at <a href="'.$postURL.'">'.$postURL.'</a></p>';
+	}
+	else{
+		print '<h1>Post could not be saved</h1>';
+		print '<p>The post could not be saved, likely because Gnat does not have write access to the location. 
+		Click the continue button to be taken back to the post editor so that you can copy and paste your post 
+		somewhere else to save it until the problem can be fixed.</p>';
+	}
 
 
 ?>
