4c6a6a3e2af4dfc6efe0a036a2af7b7a602971a4
Nate Levesque <public@thenaterhood.com>
2013-06-06 02:32:32 -0400

    preset length to NULL

--- engine/class_varGetter.php
+++ engine/class_varGetter.php
@@ -21,7 +21,7 @@ class varGetter extends sanitation{
 	  * 
 	  * @param $name - the name of the variable
 	  */	 
-	 public function __construct( $name ){
+	 public function __construct( $name, $length=NULL ){
 		 
 		 if ( ! $length ){ // If no length specified, find the default
 			 $conf = getConfigOption( $name );
