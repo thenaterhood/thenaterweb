38917dd869553a36237519e64484ae5d4399a1da
Nate Levesque <public@thenaterhood.com>
2013-06-06 20:54:21 -0400

    improvements to code style, now edits conf via editblog

--- engine/webadmin/static/page_newblog.php
+++ engine/webadmin/static/page_newblog.php
@@ -1,4 +1,3 @@
-<h1>Create New Blog</h1>
 <?php
 
 if ( isset( $_POST['blogid'] ) ){
@@ -12,6 +11,7 @@ if ( isset( $_POST['blogid'] ) ){
 
 	if ( is_writable( $localpath.'/writetest.txt') ){
 
+		unlink( $localpath.'/writetest.txt' );
 		$config = file_get_contents('templates/newblog/class_blogdef.php');
 		$lines = count( explode( "\n", $config) )+4;
 
@@ -28,13 +28,9 @@ if ( isset( $_POST['blogid'] ) ){
 		copy( 'templates/newblog/feed.php', $localpath.'/feed.php' );
 		copy( 'templates/newblog/index.php', $localpath.'/index.php' );
 
-		print '<form action="index.php?id=saveconf" method="post">
-			<input type="hidden" name="rcfile" value="../../'.$_POST['blogid'].'/class_blogdef.php"/>
-			<br />
-			<textarea name="content" rows="'.$lines.'" cols="100" >'.$config.'</textarea>
-			<br />
-			<input type="submit" value="Save and Apply" />
-			</form>';
+		$redirect = new redirect( "newblog", "index.php?id=editblog&blogid=".$_POST['blogid'] );
+		$redirect->apply( 302 );
+
 	}
 	else{
 		print '<p>Gnat does not have write access to the required locations to create a new blog.</p>';
@@ -42,6 +38,8 @@ if ( isset( $_POST['blogid'] ) ){
 
 }
 else{
+
+	print '<h1>Create New Blog</h1>';
 
 	print '<form name="create" action="index.php?id=newblog" method="post">
 	Please enter a blog name to edit: <input type="text" name="blogid" />
