ddd92ae4a90cb2b4acddede9e9beb04462c96134
Nate Levesque <public@thenaterhood.com>
2013-06-06 20:55:21 -0400

    improvements to code style

--- engine/webadmin/static/page_savepost.php
+++ engine/webadmin/static/page_savepost.php
@@ -1,67 +1,67 @@
 <?php
 /**
- * Encodes and saves an article into the 
- * appropriate location. Just a basic script,
- * nothing too fancy.
- *
- * @since 06/05/2013
- */
-	$postpath = '../../'.$_POST['blog'].'/entries';
+* Encodes and saves an article into the 
+* appropriate location. Just a basic script,
+* nothing too fancy.
+*
+* @since 06/05/2013
+*/
+$postpath = '../../'.$_POST['blog'].'/entries';
 
-	if ( $_POST['file'] == '' ){
-		$postFname = date("Y.m.d").'.json';
-		$nodename = date("Y.m.d");
-	}
+if ( $_POST['file'] == '' ){
+	$postFname = date("Y.m.d").'.json';
+	$nodename = date("Y.m.d");
+}
 
-	else{
-		$postFname = $_POST['file'];
-		$nodename = substr($postFname, 0, strpos($postFname, '.json') );
-	}
+else{
+	$postFname = $_POST['file'];
+	$nodename = substr($postFname, 0, strpos($postFname, '.json') );
+}
 
 
-	$postData = array();
+$postData = array();
 
-	$postData['content'] = $_POST['content'];
-	$postData['title'] = $_POST['title'];
-	$postData['date'] = $_POST['date'];
-	$postData['tags'] = $_POST['tags'];
-	$postData['datestamp'] = date(DATE_ATOM);
-	$postData['updated'] = date(DATE_ATOM);
+$postData['content'] = $_POST['content'];
+$postData['title'] = $_POST['title'];
+$postData['date'] = $_POST['date'];
+$postData['tags'] = $_POST['tags'];
+$postData['datestamp'] = date(DATE_ATOM);
+$postData['updated'] = date(DATE_ATOM);
 
-	$postJsonData = json_encode($postData);
-	$postFile = $postpath.'/'.$postFname;
+$postJsonData = json_encode($postData);
+$postFile = $postpath.'/'.$postFname;
 
-	$lock = new lock( $postFile );
+$lock = new lock( $postFile );
 
-	$postURL = getConfigOption('site_domain').'/'.$_POST['blog'].'/index.php?id=post&node='.$nodename;
-	$writetest = fopen( $postpath.'/writetest.txt', 'w' );
-	fclose( $writetest );
+$postURL = getConfigOption('site_domain').'/'.$_POST['blog'].'/index.php?id=post&node='.$nodename;
+$writetest = fopen( $postpath.'/writetest.txt', 'w' );
+fclose( $writetest );
 
-	if ( is_writeable( $postpath.'/writetest.txt' ) && !$lock->isLocked() ){
+if ( is_writeable( $postpath.'/writetest.txt' ) && !$lock->isLocked() ){
 
-		$lock->lock();
+	$lock->lock();
 
-		$jsonFile = fopen($postpath.'/'.$postFname, 'w');
-		fwrite($jsonFile, $postJsonData);
-		fclose($jsonFile);
+	$jsonFile = fopen($postpath.'/'.$postFname, 'w');
+	fwrite($jsonFile, $postJsonData);
+	fclose($jsonFile);
 
-		$lock->unlock();
+	$lock->unlock();
 
-		unlink( $postpath.'/writetest.txt');
+	unlink( $postpath.'/writetest.txt');
 
-		print '<h1>Post Saved</h1>';
-		print '<p>View this post at <a href="'.$postURL.'">'.$postURL.'</a></p>';
-	}
-	else{
-		print '<h1>Post could not be saved</h1>';
-		print '<p>The post could not be saved, likely because Gnat does not have write access to the location. 
-		Your post is displayed below as entered so that you can copy and paste it elsewhere until the problem gets fixed.</p>
-		<hr />';
-		print '<h1>'.htmlspecialchars( $_POST['title'] ).'</h1>';
-		print '<p>'.htmlspecialchars( $_POST['date'] ).'</p>';
-		print '<p>'.htmlspecialchars( $_POST['tags'] ).'</p>';
-		print '<p>'.htmlspecialchars( $_POST['content'] ).'</p>';
-	}
+	print '<h1>Post Saved</h1>';
+	print '<p>View this post at <a href="'.$postURL.'">'.$postURL.'</a></p>';
+}
+else{
+	print '<h1>Post could not be saved</h1>';
+	print '<p>The post could not be saved, likely because Gnat does not have write access to the location. 
+	Your post is displayed below as entered so that you can copy and paste it elsewhere until the problem gets fixed.</p>
+	<hr />';
+	print '<h1>'.htmlspecialchars( $_POST['title'] ).'</h1>';
+	print '<p>'.htmlspecialchars( $_POST['date'] ).'</p>';
+	print '<p>'.htmlspecialchars( $_POST['tags'] ).'</p>';
+	print '<p>'.htmlspecialchars( $_POST['content'] ).'</p>';
+}
 
 
 
