18eaaab5ca46afc9c6fc9de2151aa4304de61d5d
Nate Levesque <public@thenaterhood.com>
2013-06-14 20:16:04 -0400

    fix issue with retrieving saved feed

---
+++ engine/class_stdClassArticle.php
@@ -0,0 +1,20 @@
+<?php
+
+include_once 'class_article.php';
+
+class stdClassArticle extends article{
+	
+	function __construct( $stdClass ){
+
+		$this->container['title'] = $stdClass->title;
+		$this->container['tags'] = $stdClass->tags;
+		$this->container['link'] = $stdClass->link;
+		$this->container['content'] = $stdClass->content;
+		$this->container['datestamp'] = $stdClass->datestamp;
+		$this->container['date'] = $stdClass->date;
+
+	}
+
+}
+
+?>
