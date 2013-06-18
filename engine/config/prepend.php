713ece991e323d1b943f0eaba196f5c8357e1d7f
Nate Levesque <public@thenaterhood.com>
2013-06-18 00:26:52 -0400

    add prepend file

---
+++ engine/config/prepend.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * This file defines a path constant to the root
+ * of the PHP engine. It is prepended to all PHP
+ * files. It must be configured prior to running
+ * the Gnat web engine.
+ */
+
+ define("GNAT_ROOT", "/var/www/engine");
+
+ ?>
