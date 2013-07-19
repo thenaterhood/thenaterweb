99e921b5b44d0412fa8852deeeabee7c5e90e13c
Nate Levesque <public@thenaterhood.com>
2013-07-19 14:35:03 -0400

    updates for html validity - encode urls with htmlentities

--- engine/lib/pages/page_titles.php
+++ engine/lib/pages/page_titles.php
@@ -7,8 +7,13 @@ echo '<br />';
 echo '<ul>';
 $inventory = new inventory( $blogdef->post_directory );
 $titles = $inventory->selectAll();
+$year = 0;
 foreach ($titles as $item) {
-	print '<li><a href="'.$item['link'].'">'.$item['title'].'</a>';
+	if ( substr($item['datestamp'], 0, 4) != $year ){
+		$year = substr($item['datestamp'], 0, 4 );
+		print '<li><strong>'.$year.'</strong></li>';
+	}
+	print '<li><a href="'.htmlentities( $item['link'] ).'">'.$item['title'].'</a></li>';
 	# code...
 }
 echo '</ul>';
