984c6110b392832b9eb25196a985f1640c320d17
Nate Levesque <public@thenaterhood.com>
2013-07-21 03:10:57 -0400

    update to allow for inventory statistics and histograms
    update inventory class to add ability to create array of field statistics, add lib for generating histograms and new blog page for displaying tag histograms

--- engine/lib/pages/page_taghistogram.php
+++ engine/lib/pages/page_taghistogram.php
@@ -9,6 +9,7 @@
  * 	to be called standalone and should be included in another file
  * 	that already has established config and session instances.
  */
+include_once GNAT_ROOT.'/lib/core_histogram.php';
 
 $tag = $session->tag;
 $inventory = new inventory( $blogdef->post_directory );
@@ -19,28 +20,35 @@ if ( $tag ){
 	 * If a tag was requested, display which tag is being browsed
 	 * with a link back to the main page of tags
 	 */
-    print '<p><strong>Browsing tag "'.$tag.'".  </strong><a href="?id=taghistogram">View all tags.</a></p>'."\n";
+    print '<p><strong>Browsing tag "'.$tag.'".  </strong><a href="?id=tags">View all tags.</a></p>'."\n";
 
 }
-echo "<ul>";
 
 if ( !$tag ){
     /*
      * Check if a tag was requested, otherwise list the available
      * tags.
      */
-    $tags = $inventory->selectField( 'tags' );
-    sort($tags);
+    echo "<table>";
+
+    $tags = $inventory->getFieldStatistics( 'tags' );
+    ksort($tags);
+    $histogram = generateHistogramNospace( $tags, "&#9646;" );
     $letter = "";
-    foreach( $tags as $item ){
+    foreach( $histogram as $item => $graph ){
         if ( substr($item, 0, 1) != $letter ){
             $letter = substr($item, 0, 1);
-            print '<li><a id="alph_'.$letter.'"></a><strong>'.$letter.'</strong></li>'."\n";
+            print '<tr><td><a id="alph_'.$letter.'"></a><strong>'.$letter.'</strong></td></tr>'."\n";
         }
-        echo '<li><a href="index.php?id=tags&amp;tag='.$item.'">'.$item.'</a></li>'."\n";
+        echo '<tr><td><a href="index.php?id=tags&amp;tag='.$item.'">'.$item.'</a></td>';
+        echo '<td>'.$graph.'<em> '.$tags[$item].' posts</em></td></tr>'."\n";
     }
+
+    echo "</table>";
+
 }
 else{
+    echo "<ul>";
     /*
      * If a tag was requested, call getByTag to print all posts
      * with the tag.
@@ -51,8 +59,9 @@ else{
         print '<li><a href="'.htmlentities( $item['link'] ).'">'.$item['title'].'</a></li>';
     }
 
+    echo "</ul>";
+
 }
 
-echo "</ul>";
 
 ?>
