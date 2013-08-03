0c5562c960c62cb302f9a48513f6b37f37fb4fb7
Nate Levesque <public@thenaterhood.com>
2013-08-03 10:31:12 -0400

    change phpdoc template

--- tools/mkphpdoc.sh
+++ tools/mkphpdoc.sh
@@ -1,1 +1,1 @@
-phpdoc -t ../documentation/phpdoc/ -d ../engine/lib -d ../engine/classes -d ../engine/config --title thenaterhood.com --template "abstract"
+phpdoc -t ../documentation/phpdoc/ -d ../engine/lib -d ../engine/classes -d ../engine/config --title thenaterhood.com --template "zend"
