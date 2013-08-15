2d7a2e2b02a967964a3080b5f4070ea4e249e6cb
Nate Levesque <public@thenaterhood.com>
2013-08-14 22:56:54 -0400

    add devrants blog to Production

---
+++ devrants/feed.php
@@ -0,0 +1,13 @@
+<?php
+
+include GNAT_ROOT.'/lib/core_feed.php';
+include 'class_blogdef.php';
+
+$session = new session( array('regen') );
+$config = new config();
+$blogdef = new blogdef();
+
+$feed = generateFeed( $blogdef->id, $blogdef->title, $blogdef->catchline, $session->regen, $blogdef->post_directory );
+print $feed->output( $config->feed_type );
+
+?>
