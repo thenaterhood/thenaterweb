708a4b1b494beac376ed872318d0b808999978c8
Nate Levesque <public@thenaterhood.com>
2013-08-16 23:39:55 -0400

    update to use new content system

--- index.php
+++ index.php
@@ -4,12 +4,13 @@ include GNAT_ROOT.'/lib/core_redirect.php';
 
 $session = new session( array('name', 'track', 'konami', 'id', 'type') );
 $config = new config();
-$registerExtensions = array( );
+$registerExtensions = array( "evenExtension" );
 $extensions = loadExtensions( $session, $registerExtensions );
 
 # Grab variables from the URL. Syntax for this is...
 # name of variable, default value of variable, maxlength of variable
 $id = $session->id;
+$tagline = "";
 $type = '404';
 
 if ( $config->friendly_urls ){
@@ -42,25 +43,20 @@ include $config->webcore_root.'/html_head.html';
 				<div class="entry">
 					
 				<?php 
+
 				print getPreface( $extensions );
 
-				$include = getContent( $config->webcore_root.'/page_'.$session->id, $config->webcore_root.'/template_error.php');
+				$content = pullContent( array( 'static/page_'.$session->id, 'static/hidden_'.$session->id ) );
 
-				print $include['pre'];
-
-				if ( !$include['sanitize'] ){
-					include $include['file'];
+				if ( ! $content->isPhp() ){
+					print $content->toHtml();
 				}
 				else{
-					print htmlspecialchars( file_get_contents($include['file']) );
+					include $content->getFile();
 				}
 
-				print $include['post'];
-				//$content = new content( $session->id, $session );
-				//$content->output();
-
 				print getPost( $extensions );
-
+				
 				?>
 				
 				</div>
