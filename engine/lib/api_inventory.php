4c46322307eb9a42532dd5778678bae503932645
Nate Levesque <public@thenaterhood.com>
2013-08-23 22:32:04 -0400

    improve abstraction of inventory api

--- engine/lib/api_inventory.php
+++ engine/lib/api_inventory.php
@@ -14,11 +14,11 @@ include_once GNAT_ROOT.'/classes/class_inventory.php';
 
 // This allows for a place to be set, although it's messy.
 // This api is basically just for testing the inventory functionality
-$session = new session( array( "place", "element", "inventory" ) );
+$session = new session( array( "field", "value", "inventory" ) );
 
 $inventory = new inventory( $session->inventory );
 
-$matching = $inventory->select( 'title', "Lovin' those Facebook Likes" );
+$matching = $inventory->select( $session->field, $session->value );
 
 $filledData['title'] = $matching[0]->title;
 $filledData['tags'] = $matching[0]->tags;
