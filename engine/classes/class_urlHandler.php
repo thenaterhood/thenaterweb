c3a20df6e7153bdd1b822652de8bc99fc2b6a965
Nate Levesque <public@thenaterhood.com>
2013-09-19 13:06:14 -0400

    fix include issues again

--- engine/classes/class_urlHandler.php
+++ engine/classes/class_urlHandler.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once GNAT_ROOT.'/lib/core_web.php';
+include_once GNAT_ROOT.'/lib/core_blog.php';
 
 class urlHandler{
 
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 #error_reporting(E_ALL);
 #ini_set( 'display_errors','1'); 
-include_once GNAT_ROOT.'/classes/class_urlHandler.php';
+include_once GNAT_ROOT.'/lib/core_blog.php';
 
 $urlHandler = new urlHandler();
 $sectionId = $urlHandler->getControllerId();
