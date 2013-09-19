07e4f4e6c0f8ddd3e4bccdbfbada0f32626ac860
Nate Levesque <public@thenaterhood.com>
2013-09-19 12:58:31 -0400

    fix class error

--- controller/control_mainsite.php
+++ controller/control_mainsite.php
@@ -4,15 +4,17 @@ include_once GNAT_ROOT.'/controller/interface_controller.php';
 class controller extends controllerBase{
 
 	private $id;
-	private $configFile = GNAT_ROOT.'/config/section.d/mainsite.conf.xml';
+	private $configFile;
 
 	public function __construct(){
 
-
+		$configFile = GNAT_ROOT.'/config/section.d/mainsite.conf.xml';
 		$this->readConfig( $this->configFile );
 
 
 	}
+
+
 }
 
 ?>
