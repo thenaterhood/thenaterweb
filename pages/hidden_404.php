62d6977c93cc916b2f65e88445227894a685f9e1
Nate Levesque <public@thenaterhood.com>
2013-09-21 21:42:13 -0400

    add missing slash in 404 page

--- pages/hidden_404.php
+++ pages/hidden_404.php
@@ -1,3 +1,3 @@
-<h2>Holy 404, Batman!<h2>
+<h2>Holy 404, Batman!</h2>
 <p>Sorry, but that page doesn't exist on this website. This may be the result of a misspelled web address or site restructuring. You might want to <a href='/index.php'>go home</a>.</p>
 	
