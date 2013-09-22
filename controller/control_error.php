11759a7f5f49ee15d50051ef3221c722ef783e49
Nate Levesque <public@thenaterhood.com>
2013-09-22 00:43:50 -0400

    fix again

--- controller/control_error.php
+++ controller/control_error.php
@@ -16,18 +16,20 @@ class controller extends controllerBase{
 		$type = "err404";
 		if ( $errorSession->type != '' )
 			$type = "err".$errorSession->type;
-
+		
 		$this->$type();
 
 
 	}
 
 	private function err404(){
+		$_GET['id'] = '404';
 		header('HTTP/1.0 404 Not Found');
 		
 	}
 
 	private function err403(){
+		$_GET['id'] = '403';
 		header('HTTP/1.0 403 Forbidden');
 
 	}
