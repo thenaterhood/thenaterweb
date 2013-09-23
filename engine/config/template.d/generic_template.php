3b76ff4fd7119c5386df0762b9586676517fe261
Nate Levesque <public@thenaterhood.com>
2013-09-23 10:14:44 -0400

    update footer text

--- engine/config/template.d/generic_template.php
+++ engine/config/template.d/generic_template.php
@@ -194,7 +194,7 @@ setcookie('track',$session->track,time() + (86400 * 30),"/",$session->domain); /
 
     <div id="footer">
       <div class="container">
-        <p class="muted credit">Example courtesy <a href="http://martinbean.co.uk">Martin Bean</a> and <a href="http://ryanfait.com/sticky-footer/">Ryan Fait</a>.</p>
+        <p class="muted credit">Template based on <a href="http://getbootstrap.com">Bootstrap</a>. Copyright 2012-2013 Nate Levesque (TheNaterhood). <a href="/sitemap.php">View sitemap</a>.</p>
       </div>
     </div>
 
