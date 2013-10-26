f410ac79d1401ec72e495b83ffa9baaf6ef3cc45
Nate Levesque <public@thenaterhood.com>
2013-10-26 12:06:26 -0400

    typo fixes and improve unwritable warning

--- engine/lib/webadmin/pages/page_editblog.php
+++ engine/lib/webadmin/pages/page_editblog.php
@@ -1,4 +1,4 @@
-<h1>Edit Blog Configuration</h1>
+<h1>Edit Controller Configuration</h1>
 <p><strong>WARNING:</strong> proper PHP syntax must be observed, or your edits will crash blog! These tools are very primitive and will not validate your settings or your input.</p>
 <?php
 /**
@@ -11,13 +11,17 @@ if ( $admSession->blogid ){
 	$config = file_get_contents(GNAT_ROOT.'/config/section.d/'.$session->blogid.'.conf.xml');
 	$lines = count( explode( "\n", $config) )+4;
 
-	if ( ! is_writable(GNAT_ROOT.'/config/section.d/'.$session->blogid.'.conf.xml') )
-		print '<p>Warning: Gnat cannot write to the configuration file selected, settings cannot be saved.</p>';
+	if ( ! is_writable(GNAT_ROOT.'/config/section.d/'.$admSession->blogid.'.conf.xml') )
+		print '	<div class="alert alert-warning">
+		<button type="button" class="close" data-dismiss="alert">&times;</button>
+		Thenaterweb is unable to write to this configuration file. Your settings cannot 
+		be saved.
+		</div>';
 
 	print '<form action="'.getConfigOption('site_domain').'/webadmin/saveconf" method="post">
 		<input type="hidden" name="rcfile" value="'.GNAT_ROOT.'/config/section.d/'.$session->blogid.'.conf.xml"/>
 		<br />
-		<textarea name="content" rows="'.$lines.'" cols="100" >'.$config.'</textarea>
+		<textarea name="content" rows="'.$lines.'" cols="400" >'.$config.'</textarea>
 		<br />
 		<input type="submit" value="Save and Apply" />
 		</form>';
