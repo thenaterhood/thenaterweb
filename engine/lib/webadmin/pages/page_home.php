e5f5509bc778a652894b29d555fd296a0496b2ef
Nate Levesque <public@thenaterhood.com>
2013-10-26 15:16:24 -0400

    add tango 22x22 icons

--- engine/lib/webadmin/pages/page_home.php
+++ engine/lib/webadmin/pages/page_home.php
@@ -1,9 +1,15 @@
 
 <h4>Administering <?php print getConfigOption('site_domain'); ?></h4>
 <ul>
-<li><a href="<?php print getConfigOption('site_domain'); ?>/webadmin/newpost">Create a new Post or Article</a></li>
-<li><a href="<?php print getConfigOption('site_domain'); ?>/webadmin/editpost">Edit an existing post or article</a></li>
-<li><a href="<?php print getConfigOption('site_domain'); ?>/webadmin/newblog">Create a new blog or article collection</a></li>
-<li><a href="<?php print getConfigOption('site_domain'); ?>/webadmin/editblog">Edit an existing blog or article configuration</a></li>
-<li><a href="<?php print getConfigOption('site_domain'); ?>/webadmin/editconf">Modify engine configuration</a></li>
+<li style="list-style-image: url('/assets/tango/22x22/emblems/emblem-system.png')">
+	<a href="<?php print getConfigOption('site_domain'); ?>/webadmin/newpost">Create a new Post or Article</a>
+</li>
+<li style="list-style-image: url('/assets/tango/22x22/emblems/emblem-system.png')">
+	<a href="<?php print getConfigOption('site_domain'); ?>/webadmin/editpost">Edit an existing post or article</a>
+</li>
+<li style="list-style-image: url('/assets/tango/22x22/emblems/emblem-system.png')">
+	<a href="<?php print getConfigOption('site_domain'); ?>/webadmin/editblog">Edit an existing controller configuration</a>
+</li>
+<li style="list-style-image: url('/assets/tango/22x22/emblems/emblem-system.png')">
+	<a href="<?php print getConfigOption('site_domain'); ?>/webadmin/editconf">Modify engine configuration</a></li>
 </ul>
