caf8bcbc1346160e530fd7c964a58b7065b15be5
Nate Levesque <public@thenaterhood.com>
2013-10-27 22:35:49 -0400

    add controllerBase import to index.php

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
 #error_reporting(E_ALL);
 #ini_set( 'display_errors','1'); 
- define("GNAT_ROOT", "engine");
+define("GNAT_ROOT", "engine");
+
 include_once GNAT_ROOT.'/lib/core_blog.php';
+include_once GNAT_ROOT.'/lib/interface_controller.php';
 
 $urlHandler = new urlHandler();
 $sectionId = $urlHandler->getControllerId();
