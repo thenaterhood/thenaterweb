117615ce1664a4f7d23e1c28d17dbe9218ac7978
Nate Levesque <public@thenaterhood.com>
2013-10-31 15:24:22 -0400

    update constant issue (constants cant hold array)

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 #error_reporting(E_ALL);
 #ini_set( 'display_errors','1'); 
 define("GNAT_ROOT", "engine");
-define("ENGINE_BUILTINS", array('feed') );
+$ENGINE_BUILTINS = array( 'feeds' );
 
 
 # These include the core utilities that Thenaterweb requires.
