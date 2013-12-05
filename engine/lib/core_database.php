4b71798def1627d1d465a23c478018aba1b6b214
Nate Levesque <public@thenaterhood.com>
2013-12-05 00:44:44 -0500

    add better database layer

---
+++ engine/lib/core_database.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Combines all of the database includes
+ * and miscellaneous functions into a single includable
+ * file.
+ */
+
+include_once GNAT_ROOT.'/lib/extern/php-database/Database.php';
+include_once GNAT_ROOT.'/lib/extern/php-database/ErrorStack.php';
+
+
+?>
