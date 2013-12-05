8ff6c72b893b78188eb0093556a9d2d571c2d27a
Nate Levesque <public@thenaterhood.com>
2013-12-05 00:59:27 -0500

    reconfigure core_database to configure db via conf

--- engine/lib/core_database.php
+++ engine/lib/core_database.php
@@ -4,6 +4,12 @@
  * and miscellaneous functions into a single includable
  * file.
  */
+DEFINE ('DB_TYPE', $CONFIG->engine_storage_db);
+DEFINE ('DB_USER', $CONFIG->db_user);
+DEFINE ('DB_PASSWORD', $CONFIG->db_password);
+DEFINE ('DB_HOST', $CONFIG->db_host);
+DEFINE ('DB_NAME', $CONFIG->db_name);
+DEFINE ('ERROR_LEVEL', $CONFIG->db_error_level);
 
 include_once GNAT_ROOT.'/lib/extern/php-database/Database.php';
 include_once GNAT_ROOT.'/lib/extern/php-database/ErrorStack.php';
