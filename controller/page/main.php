7a646b5de2653bc169f45f71251fa01fa84523d0
Nate Levesque <public@thenaterhood.com>
2013-12-04 21:28:08 -0500

    remove builtin page path from page controller

--- controller/page/main.php
+++ controller/page/main.php
@@ -18,7 +18,7 @@ class page extends controllerBase{
 
 		$this->pageData['session'] = $session;
 		$this->pageData['static'] = $this->page_directory;
-		$content = pullContent( array( $this->page_directory.'/page_'.$session->id, $this->page_directory.'/hidden_'.$session->id, GNAT_ROOT.'/lib/pages/page_'.$session->id ) );
+		$content = pullContent( array( $this->page_directory.'/page_'.$session->id, $this->page_directory.'/hidden_'.$session->id ) );
 		$this->pageData['content'] = $content;
 		$this->pageData['id'] = $content->title;
 		$this->pageData['title'] = $this->title;
