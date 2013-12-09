6870e004418fdd12c75f532b6c03102d55dec2e5
Nate Levesque <public@thenaterhood.com>
2013-12-09 16:27:53 -0500

    add login page template

---
+++ engine/lib/builtins/auth/pages/login.php
@@ -0,0 +1,5 @@
+<form name="login" action="/auth/login" method="post">
+	Username: <input type="text" name="username" maxlength="30" />
+	Password: <input type="password" name="pass" />
+	<input type="submit" value="Register" />
+</form>
