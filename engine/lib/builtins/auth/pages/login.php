974ce4b009cb714d5b32d63b2c9322edefe543d7
Nate Levesque <public@thenaterhood.com>
2013-12-09 17:01:10 -0500

    final change so i can tolerate it

--- engine/lib/builtins/auth/pages/login.php
+++ engine/lib/builtins/auth/pages/login.php
@@ -1,9 +1,9 @@
 <form name="login" action="/auth/login" method="post">
-	<input type='hidden' name='<?php echo $pageData['csrf_id']; ?>' value='<?php echo $pageData['csrf_token']; ?>' />
-	Username: <input type="text" name="username" maxlength="30" />
-	Password: <input type="password" name="pass" />
-	<br />
-	<input type="submit" class='btn btn-success' value="Continue" />
+        <input type='hidden' name='<?php echo $pageData['csrf_id']; ?>' value='<?php echo $pageData['csrf_token']; ?>' />
+ 		<input type="text" name="username" maxlength="30" placeholder='Username' />
+		<input type="password" name="pass" placeholder='Password' />
+        <br /><br />
+        <input type="submit" class='btn btn-success' value="Continue" />
 </form>
 
 <br />
