67aa3ef696209b0d89a43ff1f6e74677f1733a95
Nate Levesque <public@thenaterhood.com>
2013-12-09 16:47:29 -0500

    fix formatting weirdness

--- engine/lib/builtins/auth/pages/login.php
+++ engine/lib/builtins/auth/pages/login.php
@@ -1,7 +1,6 @@
 <form name="login" action="/auth/login" method="post">
 	<input type='hidden' name='<?php echo $pageData['csrf_id']; ?>' value='<?php echo $pageData['csrf_token']; ?>' />
 	Username: <input type="text" name="username" maxlength="30" />
-	<br />
 	Password: <input type="password" name="pass" />
 	<input type="submit" value="Continue" />
 </form>
