18f5414fbe9ce35e8ab3013e735e09c2cbe35278
Nate Levesque <public@thenaterhood.com>
2013-12-09 23:45:36 -0500

    fix url related problems

--- controller/blog/pages/page_manage.php
+++ controller/blog/pages/page_manage.php
@@ -3,12 +3,12 @@
 
 <ul>
 <li style="list-style-image: url('/assets/tango/22x22/actions/document-new.png')">
-	<a href="<?php print getConfigOption('site_domain').'/'.$pageData['id'].'/manage'; ?>/newpost">Create a new post or article</a>
+	<a href="<?php print getConfigOption('site_domain').'/'.$pageData['id'].'/newpost'; ?>">Create a new post or article</a>
 </li>
 <li style="list-style-image: url('/assets/tango/22x22/apps/accessories-text-editor.png')">
-	<a href="<?php print getConfigOption('site_domain').'/'.$pageData['id'].'/manage'; ?>/editpost">Edit an existing post or article</a>
+	<a href="<?php print getConfigOption('site_domain').'/'.$pageData['id'].'/editpost'; ?>">Edit an existing post or article</a>
 </li>
 <li style="list-style-image: url('/assets/tango/22x22/emblems/emblem-system.png')">
-	<a href="<?php print getConfigOption('site_domain').'/'.$pageData['id'].'/manage'; ?>/editblog">Edit blog configuration</a>
+	<a href="<?php print getConfigOption('site_domain').'/'.$pageData['id'].'/editblog'; ?>">Edit blog configuration</a>
 </li>
 </ul>
