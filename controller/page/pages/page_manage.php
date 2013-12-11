367050c0ab24aa7f99cffe9f2980db23b9b9ceec
Nate Levesque <public@thenaterhood.com>
2013-12-11 00:12:27 -0500

    fix template issues

--- controller/page/pages/page_manage.php
+++ controller/page/pages/page_manage.php
@@ -19,7 +19,7 @@
 		print '</td>';
 
 		print '<td>';
-		print '<a class="btn btn-success" href="'.$pageData['apphome'].'/edit/page/'.$pathinfo['filename'].'">Edit</a>';
+		print '<a class="btn btn-success" href="'.$pageData['apphome'].'/?url=edit/page/'.$pathinfo['filename'].'">Edit</a>';
 		print '</td>';
 		print '<td>';
 		print '<a class="btn btn-success" href="'.$page.'">View</a>';
@@ -29,3 +29,5 @@
 	
 	}
 	?>
+
+</table>
