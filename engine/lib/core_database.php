137c69473b26381129364cfde157270d44066294
Nate Levesque <public@thenaterhood.com>
2014-01-13 18:14:57 -0500

    change case of define calls

--- engine/lib/core_database.php
+++ engine/lib/core_database.php
@@ -3,17 +3,18 @@
  * Combines all of the database includes
  * and miscellaneous functions into a single includable
  * file.
+ * 
  */
 
 include_once NWEB_ROOT.'/classes/class_engine.php';
 
-DEFINE ('DB_TYPE',      Engine::get_option( 'engine_storage_db' ) );
-DEFINE ('DB_USER',      Engine::get_option( 'db_user' ) );
-DEFINE ('DB_PORT',      Engine::get_option( 'db_port' ) );
-DEFINE ('DB_PASSWORD',  Engine::get_option( 'db_password' ) );
-DEFINE ('DB_HOST',      Engine::get_option( 'db_host' ) );
-DEFINE ('DB_NAME',      Engine::get_option( 'db_name' ) );
-DEFINE ('ERROR_LEVEL',  Engine::get_option( 'db_error_level' ) );
+define('DB_TYPE',      Engine::get_option( 'engine_storage_db' ) );
+define('DB_USER',      Engine::get_option( 'db_user' ) );
+define('DB_PORT',      Engine::get_option( 'db_port' ) );
+define('DB_PASSWORD',  Engine::get_option( 'db_password' ) );
+define('DB_HOST',      Engine::get_option( 'db_host' ) );
+define('DB_NAME',      Engine::get_option( 'db_name' ) );
+define('ERROR_LEVEL',  Engine::get_option( 'db_error_level' ) );
 
 include_once NWEB_ROOT.'/lib/extern/php-database/Database.php';
 include_once NWEB_ROOT.'/lib/extern/php-database/ErrorStack.php';
