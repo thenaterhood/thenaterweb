67f1d2017c796f788689acb4e581977735ee0daa
Nate Levesque <public@thenaterhood.com>
2014-03-27 10:19:03 -0400

    improvements to error handling (less ugly)

--- engine/classes/class_engineErrorHandler.php
+++ engine/classes/class_engineErrorHandler.php
@@ -13,7 +13,40 @@ class EngineErrorHandler {
     
     	public static function handle_exception( $e ){
 
+    		if ( $e->getCode() >= 100 && $e->getCode() <= 505 ){
+    			http_response_code( $e->getCode() ) ;
+    			self::handle_http_exception( $e );
+    		} else { 
+    			header( http_response_code(500) );
+    			self::handle_generic_exception( $e );
+    		}
+
+
+		}
+
+	public static function handle_http_exception( $e ){
+
 		if ( DEBUG ){
+			self::handle_generic_exception( $e );
+		} else {
+			$pageData = array();
+			$pageData['title'] = 'Holy ' . $e->getCode() . ' (error), Batman!';
+			$pageData['text'] = "Whoops, looks like a problem!";
+
+			$pageData['content'] = pullContent('/home/nate/gnat/engine/lib/html/errorpage');
+			render_php_template( NWEB_ROOT.'/config/template.d/generic_template.php', $pageData );
+
+		}
+
+
+	}
+
+	public static function handle_generic_exception( $e ){
+
+		if ( DEBUG ){
+
+			$pageData = array();
+			$pageData['title'] = "Server Error (" . $e->getCode() . ')';
 
 			$stack = array();
 			$stack[] = $e->getFile() . ' line ' . $e->getLine() . ': <strong>' . $e->getMessage() . '</strong>' . 
@@ -32,20 +65,32 @@ class EngineErrorHandler {
 
 
 
-			$pageData = array();
 			$pageData['stack'] = $stack;
-			$pageData['title'] = "Uncaught Exception (assumed fatal)";
+			$pageData['hasStack'] = true;
+			$pageData['static'] = "";
+			$pageData['id'] = "Error";
 
-			render_php_template( NWEB_ROOT.'/lib/html/error.html', $pageData );
+
+
+
 		} else {
 
-			echo "<h1>Error 500:</h1> <p>The requested page could not be loaded due to a problem encountered by the site software.</p>";
+			$pageData = array();
+			$pageData['hasStack'] = false;
+			$pageData['static'] = "";
+			$pageData['id'] = "Error";
+			$pageData['title'] = "Internal Server Error";
+			$pageData['text'] = 'Sorry, but the site software was not able to provide the page you requested due to a problem.';
+
 
 		}
+
+		$pageData['content'] = pullContent('/home/nate/gnat/engine/lib/html/errorpage');
+		render_php_template( NWEB_ROOT.'/config/template.d/generic_template.php', $pageData );
 
 		die();
 
 
 	}
-    //put your code here
+
 }
--- engine/lib/core_web.php
+++ engine/lib/core_web.php
@@ -25,6 +25,7 @@ include NWEB_ROOT.'/classes/class_request.php';
 include NWEB_ROOT.'/classes/class_dataAccessLayer.php';
 include NWEB_ROOT.'/classes/class_modelBase.php';
 include NWEB_ROOT.'/classes/class_engineErrorHandler.php';
+include NWEB_ROOT.'/lib/core_httpstatus.php';
 include_once NWEB_ROOT.'/classes/class_engine.php';
 
 /**
