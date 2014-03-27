26bb6aec4230331e522105bb5fe245d2701105b7
Nate Levesque <public@thenaterhood.com>
2014-03-27 10:15:12 -0400

    fix issue with generating feeds

--- engine/classes/class_article.php
+++ engine/classes/class_article.php
@@ -211,7 +211,7 @@ class Article extends dataMonger{
 		# statically, then urlencode the rest of the url. Otherwise, the feed does not 
 		# validate.
 		$r .= "<id>http://" . urlencode( substr($this->link, 7) ) . "</id>";
-		$r .= '<link href="http://'. htmlspecialchars( substr($this->container['link'], 7) ) .'" />';
+		$r .= '<link href="http://'. htmlspecialchars( substr($this->link, 7) ) .'" />';
 		$r .= '<updated>'.$this->datestamp.'</updated>';
 		$r .= "<title>" . htmlspecialchars( $this->title ) . "</title>";
 		$r .= "<content type='html'>" . htmlspecialchars( $this->content, ENT_QUOTES ) . "</content>";
